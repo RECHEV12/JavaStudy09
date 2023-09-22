7295e87298597d80d3cccce78f1d8b728b03c53b
RECHEV12 <hdw1202@naver.com>
2023-09-22 10:12:39 +0900

    내용 추가

--- src/ch04_operator/OperatorMain.java
+++ src/ch04_operator/OperatorMain.java
@@ -125,6 +125,13 @@ public class OperatorMain {
 
         System.out.println("\n=========================\n");
 
+        //나머지 연산자(%) 사용 예시
+
+        // 홀수 짝수 판별
+        // 2로 나누었을 때 0이면 짝수, 1이면 홀
+        int anyNum = 7;
+        System.out.println(anyNum % 2);
+
 
 
 
